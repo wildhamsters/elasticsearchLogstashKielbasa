ae33431b30325ac5c40495bbcaec3329554ab23b
Kamil Koziol <kamil_koziol@epam.com>
2022-05-17 15:32:41 +0200

    inital commit

---
+++ src/test/java/org/wildhamsters/elk/ElkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wildhamsters.elk;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
